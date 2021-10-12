4a832266211bf305c6c7e4f51e880f79e94a77bb
gachd <gersonchaparro@gmail.com>
2021-10-12 11:29:37 -0300

    test hosting

--- application/controllers/accionistas/titulos.php
+++ application/controllers/accionistas/titulos.php
@@ -1,14 +1,10 @@
-<?php
-
+<?php 
+        
 defined('BASEPATH') OR exit('No direct script access allowed');
+        
+class Titulos extends CI_Controller {
 
-
-
-class Titulos extends CI_Controller
-{
-
-
-
+  
     function __construct()
     {
 
@@ -17,21 +13,24 @@ class Titulos extends CI_Controller
         parent::__construct();
         //$this->output->enable_profiler(TRUE);
 
-        
+        $this->load->library('session');
+
         $this->load->model('model_socios');
         $this->load->model('model_libro');
         $this->load->model('model_titulo');
         $this->load->model('model_persona');
         $this->load->model('model_accionistas');
-        
-        
+
+
         $this->load->helper('url');
+
         $this->load->helper('form');
+
         $this->load->library('form_validation');
-        $this->load->library('session');
+
         $this->load->library('calendar');
 
-      
+        $this->load->library('session');
     }
 
 
@@ -184,7 +183,7 @@ class Titulos extends CI_Controller
             echo '<option value="' . $t->id_titulos . '">' . $t->id_titulos . '  ' . $t->prsn_nombres . ' ' . $t->prsn_apellidopaterno . '</option>';
         }
 
-        
+
 
 
         // print_r(json_encode($titulos));
@@ -203,7 +202,7 @@ class Titulos extends CI_Controller
             echo '<option value="' . $t->id_titulos . '">' . $t->id_titulos . '  ' . $t->prsn_nombres . ' ' . $t->prsn_apellidopaterno . '</option>';
         }
 
-        
+
 
 
         // print_r(json_encode($titulos));
@@ -519,8 +518,8 @@ class Titulos extends CI_Controller
     public function embargo()
 
     {
-        $idT=$this->input->post('idT');
-        $data["RutA"]=$this->input->post('RutA');
+        $idT = $this->input->post('idT');
+        $data["RutA"] = $this->input->post('RutA');
 
         $data["IdTitulo"] = $idT;
         $data["Titulo"] = $this->model_titulo->infoTituloID($idT);
@@ -545,60 +544,52 @@ class Titulos extends CI_Controller
         $Numero_Acciones_titulo = $this->input->post('numero_acciones');
         $embargadasOriginal = $this->input->post('acciones_embargadas');
         $TextEmbargadaVista = $this->input->post('cant_embargada');
-        $idT= $this->input->post('idT');
-        $RutA= $this->input->post('RutA');
+        $idT = $this->input->post('idT');
+        $RutA = $this->input->post('RutA');
 
         $NuevasEmbargadas = '';
 
 
 
-       $archivos=$_FILES["archivos_embargo"];
+        $archivos = $_FILES["archivos_embargo"];
 
-       $this->Subir_Varios_Embargo($RutA, $archivos);
+        $this->Subir_Varios_Embargo($RutA, $archivos);
 
 
         if ($OpEmbargo == "SI") {
 
-            $NuevasEmbargadas = $embargadasOriginal+ $TextEmbargadaVista;
-
-          
+            $NuevasEmbargadas = $embargadasOriginal + $TextEmbargadaVista;
         }
         if ($OpEmbargo == "NO") {
 
             $NuevasEmbargadas = $embargadasOriginal - $TextEmbargadaVista;
-
-            
         }
 
         if ($NuevasEmbargadas == 0) {
-            $dataT = array(         
+            $dataT = array(
 
                 'embargo' => 0,
-    
-                'acciones_embargadas' => $NuevasEmbargadas ,
-    
-            );
-        }else{
-            
 
-            $dataT = array(     
-                 
+                'acciones_embargadas' => $NuevasEmbargadas,
+
+            );
+        } else {
+
+
+            $dataT = array(
+
 
                 'embargo' => 1,
-                'acciones_embargadas' => $NuevasEmbargadas ,
-    
+                'acciones_embargadas' => $NuevasEmbargadas,
+
             );
-
-
         }
 
 
-        $this->model_titulo->updatetitulos($dataT,$idT);
+        $this->model_titulo->updatetitulos($dataT, $idT);
         $this->session->set_flashdata('embargo', 'Titulo Actualizado');
 
         redirect('accionistas/titulos');
-
-
     }
 
 
@@ -647,63 +638,67 @@ class Titulos extends CI_Controller
     //private
 
     private function Subir_Varios_Embargo($user, $archivo)
-	{
+    {
 
 
-		$formatos = array('jpg', 'png', 'jpeg', 'gif', 'pdf');
+        $formatos = array('jpg', 'png', 'jpeg', 'gif', 'pdf');
 
-		$fecha = date("Y.m.d_");
-
-
-
-		$Dir_archivos = 'archivos/accionista/'; //carpeta donde se guadaran todos los archivos subidos del sistema accionisstas.
-
-
-		foreach ($archivo['tmp_name'] as $key => $tmp_name) {
-			//condicional si el fuchero existe
-			if ($archivo["name"][$key]) {
-				// Nombres de archivos de temporales
-
-
-				$archivonombre = $fecha . $archivo["name"][$key];
-
-				$fuente = $archivo["tmp_name"][$key];
-
-				$carpeta = $Dir_archivos . $user . '/Embargo'.'/'; //Declaramos el nombre de la carpeta que guardara los archivos embargados
-
-				if (!file_exists($carpeta)) {
-
-					mkdir($carpeta, 0777, true) or die("Hubo un error al crear el directorio de almacenamiento");
-				}
-				var_dump($carpeta);
-
-				//Abrimos el directorio
-				$dir = opendir($carpeta);
-
-				$path_archivo = $carpeta . '/' . $archivonombre; //indicamos la ruta de destino de los archivos
-
-				$Tipo_archivo = pathinfo($path_archivo, PATHINFO_EXTENSION);
+        $fecha = date("Y.m.d_");
 
 
 
-				if (in_array($Tipo_archivo, $formatos)) {
-
-					if (move_uploaded_file($fuente, $path_archivo)) {
-
-						echo "El archivo $archivonombre se han cargado de forma correcta.<br>";
-					} else {
-
-						echo "Se ha producido un error, por favor revise los archivos e intentelo de nuevo.<br>";
-					}
-				} else {
-
-					echo "Formato del archivo $archivonombre no valido.<br>";
-				}
-
-				closedir($dir); //Cerramos la conexion con la carpeta destino
+        $Dir_archivos = 'archivos/accionista/'; //carpeta donde se guadaran todos los archivos subidos del sistema accionisstas.
 
 
-			}
-		}
-	}
+        foreach ($archivo['tmp_name'] as $key => $tmp_name) {
+            //condicional si el fuchero existe
+            if ($archivo["name"][$key]) {
+                // Nombres de archivos de temporales
+
+
+                $archivonombre = $fecha . $archivo["name"][$key];
+
+                $fuente = $archivo["tmp_name"][$key];
+
+                $carpeta = $Dir_archivos . $user . '/Embargo' . '/'; //Declaramos el nombre de la carpeta que guardara los archivos embargados
+
+                if (!file_exists($carpeta)) {
+
+                    mkdir($carpeta, 0777, true) or die("Hubo un error al crear el directorio de almacenamiento");
+                }
+                var_dump($carpeta);
+
+                //Abrimos el directorio
+                $dir = opendir($carpeta);
+
+                $path_archivo = $carpeta . '/' . $archivonombre; //indicamos la ruta de destino de los archivos
+
+                $Tipo_archivo = pathinfo($path_archivo, PATHINFO_EXTENSION);
+
+
+
+                if (in_array($Tipo_archivo, $formatos)) {
+
+                    if (move_uploaded_file($fuente, $path_archivo)) {
+
+                        echo "El archivo $archivonombre se han cargado de forma correcta.<br>";
+                    } else {
+
+                        echo "Se ha producido un error, por favor revise los archivos e intentelo de nuevo.<br>";
+                    }
+                } else {
+
+                    echo "Formato del archivo $archivonombre no valido.<br>";
+                }
+
+                closedir($dir); //Cerramos la conexion con la carpeta destino
+
+
+            }
+        }
+    }
 }
+
+
+
+
