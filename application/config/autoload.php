433eedb3f53af0123a4ef693f0168f5ada4c2a4b
Gerson Chaparro <gersonchaparro@gmail.com>
2021-12-14 13:02:21 -0300

    correo, juntas, directorio

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -90,7 +90,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url','form','funcionesutiles');
+$autoload['helper'] = array('url','form','funciones');
 
 /*
 | -------------------------------------------------------------------
